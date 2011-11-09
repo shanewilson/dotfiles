6f2188617fc90ed30b0ca639ff32555e7c1bb4d3
Shane Wilson <shanezilla@gmail.com>
2011-11-09 00:13:35 -0500

    emacs changes

--- emacs/init.el
+++ emacs/init.el
@@ -1,18 +1,35 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(package-initialize)
+;; -*- coding: utf-8 -*-
+(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/site-lisp/color-theme-solarized/"))
 
-(when (not package-archive-contents)
-  (package-refresh-contents))
+;;----------------------------------------------------------------------------
+;; Which functionality to enable (use t or nil for true and false)
+;;----------------------------------------------------------------------------
+(setq *macbook-pro-support-enabled* t)
+(setq *is-a-mac* (eq system-type 'darwin))
+(setq *is-carbon-emacs* (and *is-a-mac* (eq window-system 'mac)))
+(setq *is-cocoa-emacs* (and *is-a-mac* (eq window-system 'ns)))
 
-;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings clojure-mode slime-repl paredit slime)
-  "A list of packages to ensure are installed at launch.")
+;;----------------------------------------------------------------------------
+;; Make elisp more civilised
+;;----------------------------------------------------------------------------
+(require 'cl)
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+;;----------------------------------------------------------------------------
+;; Load configs for specific features and modes
+;;----------------------------------------------------------------------------
+(require 'init-utils)
+(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+(require 'init-elpa)
+(require 'init-exec-path) ;; Set up $PATH
+(require 'init-themes)
+(require 'init-misc)
+(require 'init-frame-hooks)
+(require 'init-xterm)
+(require 'init-windows)
+(require 'init-editing-utils)
+(require 'init-osx-keys)
+(require 'init-evil)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,12 +42,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(setq dotfiles-dir (file-name-directory
-                    (or (buffer-file-name) load-file-name)))
-(setq user-specific-dir (concat dotfiles-dir user-login-name))
-(add-to-list 'load-path (concat user-specific-dir "/color-theme-solarized"))
-(require 'solarized-definitions)
-(add-to-list 'custom-theme-load-path (concat user-specific-dir "/color-theme-solarized"))
-(set-frame-font "Mono-10")
-(load-theme 'solarized-dark)
